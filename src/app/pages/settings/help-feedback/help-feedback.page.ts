9b5f539cc9a28ab7bb5f2a9a1c7e4b127f46092e
Khusha3560 <khushboo.sharma@ekarigar.com>
2025-09-25 13:58:21 +0530

    added languages locale for help-feedback page

--- src/app/pages/settings/help-feedback/help-feedback.page.ts
+++ src/app/pages/settings/help-feedback/help-feedback.page.ts
@@ -2,22 +2,23 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-help-feedback',
   templateUrl: './help-feedback.page.html',
   styleUrls: ['./help-feedback.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, TranslateModule], // ✅ include TranslateModule
 })
 export class HelpFeedbackPage implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private translate: TranslateService // ✅ available if needed
+  ) {}
 
-  ngOnInit() {
-  }
-
-
+  ngOnInit() {}
 
   openHelpCenter() {
     this.router.navigate(['settings/help/help-center']);
